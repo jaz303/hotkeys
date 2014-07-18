f63a12ca442e6a42a7fc0f4e9120101636245842
Jason Frame <jason@onehackoranother.com>
2014-07-18 16:38:03 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
-	defaultKeycodes 	: require('./lib/default_keycodes'),
-	Dispatcher 			: require('./lib/Dispatcher'),
-	Hotkey 				: require('./lib/Hotkey'),
-	keycodes 			: require('./lib/keycodes'),
-	Keymap 				: require('./lib/Keymap'),
-	parseKeyCombo		: require('./lib/parse_key_combo')
+    defaultKeycodes     : require('./lib/default_keycodes'),
+    Dispatcher          : require('./lib/Dispatcher'),
+    Hotkey              : require('./lib/Hotkey'),
+    keycodes            : require('./lib/keycodes'),
+    Keymap              : require('./lib/Keymap'),
+    parseKeyCombo       : require('./lib/parse_key_combo')
 };
