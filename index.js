c24be0315ae032060c573858cad143f063ad5427
Jason Frame <jason@onehackoranother.com>
2014-07-18 16:49:47 +0100

    don’t export keycodes at top level

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ module.exports = {
     defaultKeycodes     : require('./lib/default_keycodes'),
     Dispatcher          : require('./lib/Dispatcher'),
     Hotkey              : require('./lib/Hotkey'),
-    keycodes            : require('./lib/keycodes'),
     Keymap              : require('./lib/Keymap'),
     parseKeyCombo       : require('./lib/parse_key_combo')
 };
